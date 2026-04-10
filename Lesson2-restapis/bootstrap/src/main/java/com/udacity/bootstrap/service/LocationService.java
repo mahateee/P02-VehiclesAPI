55b8c8c7301e68a2c4b3b1b779a3b670bf8be329
mmalshehri <mmalshehri@estishraf.com>
2026-04-10 19:26:10 +0300

    Remove unused lesson folders

--- Lesson2-restapis/bootstrap/src/main/java/com/udacity/bootstrap/service/LocationService.java
+++
@@ -1,9 +0,0 @@
-package com.udacity.bootstrap.service;
-
-import com.udacity.bootstrap.entity.Location;
-
-import java.util.List;
-
-public interface LocationService {
-    List<Location> retrieveLocations();
-}
